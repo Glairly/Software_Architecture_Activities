24d30584bae35aa26a062fda101b7385c0baf531
Wongvarit Pancharoen <oakbahina@gmail.com>
2021-10-10 21:13:13 +0700

    creational

---
+++ SoftArchDesign-compat/SoftArchDesign/src/main/java/creational/BookMetadataExporter.java
@@ -0,0 +1,20 @@
+package creational;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public abstract class BookMetadataExporter extends BookCollection {
+
+    BookMetadataFormatter formatter;
+
+    public void export(PrintStream stream) throws ParserConfigurationException, IOException {
+        // Please implement this method. You may create additional methods as you see fit.
+        for(var book : books){
+            formatter.append(book);
+        }
+        System.out.print(formatter.getMetadataString());
+    }
+
+
+}
